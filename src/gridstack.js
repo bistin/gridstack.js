63350a2af088a5b512cabac96406072b62b243f4
bistin <bistin@gmail.com>
2018-06-01 09:43:55 +0800

    fix animation disappear

--- src/gridstack.js
+++ src/gridstack.js
@@ -1019,8 +1019,8 @@
 
         if (typeof maxHeight == 'undefined') {
             maxHeight = this._styles._max;
+            this._initStyles();
         }
-        this._initStyles();
         this._updateContainerHeight();
         if (!this.opts.cellHeight) { // The rest will be handled by CSS
             return ;
